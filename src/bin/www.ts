bba24d5765b3c0c964388f77def653d57baa97a2
Hung Nguyen <hungnguyen@brickmate.kr>
2020-12-23 10:32:15 +0700

    TSNODE-D20201223: Add CI/CD

--- src/bin/www.ts
+++ src/bin/www.ts
@@ -10,3 +10,10 @@ import "./server";
 import "../connector/redis/index";
 import "../connector/mongo/init/index";
 import "../connector/socket.io/__test__/__test__.socket.io-client";
+
+if (process.env.CI) {
+    setTimeout(() => {
+        console.log("CI/CD Done.");
+        process.exit(1);
+    }, 30 * 1000);
+}
