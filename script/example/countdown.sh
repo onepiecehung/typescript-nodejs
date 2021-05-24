6815671843dbbc55ecc66e32f1f49668ac11f0e3
Vereenigde Oost-Indische Compagnie <hungnguyen@brickmate.kr>
2021-05-24 17:19:42 +0700

    Delete example script backup mongodb

--- script/example/countdown.sh
+++
@@ -1,9 +0,0 @@
-#/bin/ksh
-
-
-secs=$((5 * 60))
-while [ $secs -gt 0 ]; do
-   echo -ne "Excute: $secs\033[0K\r"
-   sleep 1
-   : $((secs--))
-done
--- script/example/input.sh
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/env bash
-
-echo "What's your name?"
-read name
-echo "Hi, $name."
